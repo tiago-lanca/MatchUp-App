49c44604b059bb1559d9a9a160186fe054d232c7
Tiago Lança <tiagofilipe.lanca@gmail.com>
2025-10-25 18:05:18 +0100

    Added Spring boot API for MatchUp project

---
+++ MatchUpAPI/src/main/java/com/matchup/api/matchup_api/MatchUpApiApplication.java
@@ -0,0 +1,13 @@
+package com.matchup.api.matchup_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchUpApiApplication.class, args);
+	}
+
+}
---
+++ MatchUpAPI/src/test/java/com/matchup/api/matchup_api/MatchUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchup.api.matchup_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
