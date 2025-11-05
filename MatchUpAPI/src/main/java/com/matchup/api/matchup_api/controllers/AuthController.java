8a13e5fe54b31c09b091debb77120120b0faebe9
Tiago Lança <tiagofilipe.lanca@gmail.com>
2025-11-05 01:18:05 +0000

    Created controller for authentication endpoints for login

--- MatchUpAPI/src/main/java/com/matchup/api/matchup_api/controllers/AuthController.java
+++ MatchUpAPI/src/main/java/com/matchup/api/matchup_api/controllers/AuthController.java
@@ -1,4 +1,45 @@
 package com.matchup.api.matchup_api.controllers;
 
+import com.matchup.api.matchup_api.dtos.UserDTO;
+import com.matchup.api.matchup_api.models.User;
+import com.matchup.api.matchup_api.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+
+@RequestMapping(path="/api/auth")
 public class AuthController {
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
+    private final UserRepository _userRepository;
+
+    public AuthController(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> getLogin(@RequestBody User user) {
+        logger.info("Trying to login user");
+
+        Optional<User> existingUser = _userRepository.getUserByEmail(user.getEmail());
+        if(existingUser.isPresent()){
+            User foundUser = existingUser.get();
+            if(foundUser.getPasswordHash().equals(user.getPasswordHash())){
+                var userDto = UserDTO.fromEntity(foundUser);
+                return ResponseEntity.ok(userDto);
+            } else {
+                logger.info("Invalid password for user: " + user.getEmail());
+                return ResponseEntity.status(401).body(null);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
 }
--- MatchUpAPI/src/main/java/com/matchup/api/matchup_api/repositories/UserRepository.java
+++ MatchUpAPI/src/main/java/com/matchup/api/matchup_api/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package com.matchup.api.matchup_api.repositories;
 import com.matchup.api.matchup_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
+    Optional<User> getUserByEmail(String email);
 }
