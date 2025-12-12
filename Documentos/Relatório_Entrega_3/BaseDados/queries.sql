59f5f59e548a3589ef83ae9f80798c8ae3f336ef
Tiago Lança <tiagofilipe.lanca@gmail.com>
2025-12-12 01:11:53 +0000

    Updating queries.sql

--- Documentos/Relatório_Entrega_3/BaseDados/queries.sql
+++ Documentos/Relatório_Entrega_3/BaseDados/queries.sql
@@ -38,6 +38,24 @@ ORDER BY e.eve_name, u.user_name;
 -- Média de custo dos eventos
 SELECT AVG(eve_cost) AS AvgEventCost FROM Events;
 
+-- Valor médio por desporto
+SELECT spo_name, avg(eve_cost) as ValorMedio, count(*) as TotalEventos
+FROM Events
+inner join Sports on eve_sport_id = spo_id
+group by spo_name
+
+-- Percetagem de eventos pagos x gratuis
+SELECT 
+    CAST(ROUND(100.0 * SUM(CASE WHEN eve_cost > 0 THEN 1 END) / COUNT(*), 1) AS DECIMAL(5,1)) AS PercentagemPago,
+    CAST(ROUND(100.0 * SUM(CASE WHEN eve_cost = 0 THEN 1 END) / COUNT(*), 1) AS DECIMAL(5,1)) AS PercentagemGratuito
+FROM Events;
+
+-- Média de custo de eventos por localidade
+SELECT adr_city, avg(eve_cost) as ValorMedio, count(*) as TotalEventos
+FROM Events
+inner join Addresses on eve_address_id = adr_id
+group by adr_city
+
 -- Obter numero de eventos existentes por desporto
 SELECT s.spo_name AS Sport, COUNT(e.eve_id) AS TotalEvents
 FROM Sports s
@@ -55,3 +73,24 @@ SELECT eve_name, adr_city
 FROM Events e
 JOIN Addresses a ON e.eve_address_id = a.adr_id
 WHERE adr_city LIKE N'%Lisboa';
+
+-- Quantidade de eventos por género no 'Seixal'
+SELECT eve_gender, count(*)
+FROM Events
+inner join Addresses on eve_address_id = adr_id
+where adr_city = 'Seixal'
+group by eve_gender
+
+-- Receita total de eventos por cidade
+SELECT adr_city, sum(eve_cost * eve_maxMembers) as ReceitaTotal
+FROM Events
+inner join Addresses on eve_address_id = adr_id
+group by adr_city
+order by ReceitaTotal desc
+
+-- Receita total de eventos por desporto
+SELECT spo_name, sum(eve_cost * eve_maxMembers) as ReceitaTotal
+FROM Events
+inner join Sports on eve_sport_id =spo_id
+group by spo_name
+order by ReceitaTotal desc
